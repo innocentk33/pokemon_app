8b63bc9427e4a08079cfcdd9ca7537106aca49c8
Innocent Kacou <innocentk33@gmail.com>
2020-03-05 10:47:35 +0000

    first commit

---
+++ android/app/src/main/java/com/gdgmiage/pokemonapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gdgmiage.pokemonapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
